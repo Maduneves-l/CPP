f1f1920dea2ae7861e8e52a47d5c6edc8dba32e3
Maduneves-l <neves.maedu@gmail.com>
2024-06-18 16:02:09 +0100

    done

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,16 +6,17 @@
 /*   By: mneves-l <mneves-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 21:52:17 by mneves-l          #+#    #+#             */
-/*   Updated: 2024/06/14 21:52:25 by mneves-l         ###   ########.fr       */
+/*   Updated: 2024/06/18 15:55:55 by mneves-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int main(void){
-    Zombie* horde = zombieHorde(5, "Zombie");
-    for (int i = 0; i < 5; i++)
-        horde[i].announce();
+    int num = 5;
+    Zombie* horde = zombieHorde(num, "Zombie");
+    for (int i = 0; i < num; i++)
+        horde[i].announce(); 
     delete [] horde;
     return 0;
 }
--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mneves-l <mneves-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 22:49:38 by mneves-l          #+#    #+#             */
-/*   Updated: 2024/06/18 13:30:00 by mneves-l         ###   ########.fr       */
+/*   Updated: 2024/06/18 15:58:28 by mneves-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include <fstream>
 
 void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    if (from.empty())
+        return;
     size_t start_pos = 0;
     while((start_pos = str.find(from, start_pos)) != std::string::npos) {
         str.replace(start_pos, from.length(), to);
